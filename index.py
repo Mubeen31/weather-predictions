9fb5b12986ae10bb04a2343174fdba9b89ef4370
Mubeen Ali <qs6272527@gmail.com>
2022-03-22 14:54:20 +0000

    Update index.py

--- index.py
+++ index.py
@@ -14,6 +14,8 @@ from dash import dash_table as dt
 import time
 import pathlib
 from components.data_update_time import last_data_update_time
+from components.title_image_value import title_image_and_value
+from components.background_image_container import background_image_and_container
 
 # engine = sqlalchemy.create_engine('mysql+pymysql://b54eb1e6af434b:181636f95f46e13@eu-cdbr-west-02.cleardb.net:3306/heroku_323e0ab91ec4d38')
 # df = pd.read_sql_table('accuweather', engine)
@@ -29,45 +31,51 @@ font_awesome = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
 meta_tags = [{"name": "viewport", "content": "width=device-width"}]
 external_stylesheets = [meta_tags, font_awesome]
 
-app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = "Weather Sensor Data"
 
 app.layout = html.Div([
     html.Div([
-        dcc.Interval(id = 'update_value',
-                     interval = 6000,
-                     n_intervals = 0),
+        dcc.Interval(id='update_value',
+                     interval=6000,
+                     n_intervals=0),
     ]),
 
     html.Div([
-        dcc.Interval(id = 'update_time',
-                     interval = 1000,
-                     n_intervals = 0),
+        dcc.Interval(id='update_time',
+                     interval=1000,
+                     n_intervals=0),
     ]),
 
     html.Div([
         html.Div([
             html.Div([
-                html.Div(id = 'last_data_update_time'),
-            ], className = 'last_data_update_time'),
+                html.Div(id='last_data_update_time'),
+            ], className='last_data_update_time'),
             html.Div([
                 html.Div([
                     html.Div([
-                        html.I(className = "fas fa-home"),
-                    ], className = 'title_image'),
+                        html.I(className="fas fa-home"),
+                    ], className='title_image'),
                     html.H6('Newtown Road, Worcester, England',
-                            style = {'color': 'white'},
-                            className = 'title'
+                            style={'color': 'white'},
+                            className='title'
                             ),
-                ], className = 'logo_title'),
-                html.Div(id = 'title_image_value')
-            ], className = 'title_image_value_row'),
-            html.A(href = 'https://www.accuweather.com/en/gb/worcester/wr1-3/current-weather/331595',
-                   target = '_blank',
-                   children = [html.P('AccuWeather Data: Worcester, Worcestershire, England', className = 'link')]),
-        ], className = 'title_date_time_container')
-    ], className = 'title_date_time_container_overlay'),
+                ], className='logo_title'),
+                html.Div(id='title_image_value')
+            ], className='title_image_value_row'),
+            html.A(href='https://www.accuweather.com/en/gb/worcester/wr1-3/current-weather/331595',
+                   target='_blank',
+                   children=[html.P('AccuWeather Data: Worcester, Worcestershire, England', className='link')]),
+        ], className='title_date_time_container')
+    ], className='title_date_time_container_overlay'),
+
+    html.Div([
+
+        html.Div(id='background_image_container',
+                 className='background_image'),
+    ], className='background_image_current_weather_time_column'),
 ])
 
 
@@ -79,5 +87,21 @@ def last_data_update_time_callback(n_intervals):
     return last_data_update_time_data
 
 
+@app.callback(Output('title_image_value', 'children'),
+              [Input('update_value', 'n_intervals')])
+def title_image_and_value_callback(n_intervals):
+    title_image_and_value_data = title_image_and_value(n_intervals)
+
+    return title_image_and_value_data
+
+
+@app.callback(Output('background_image_container', 'children'),
+              [Input('update_value', 'n_intervals')])
+def background_image_and_container_callback(n_intervals):
+    background_image_and_container_data = background_image_and_container(n_intervals)
+
+    return background_image_and_container_data
+
+
 if __name__ == "__main__":
-    app.run_server(debug = True)
+    app.run_server(debug=True)
